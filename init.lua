c288a9ad8164adfb3814ed309c24717fa22ac21f
Beha <shacknetisp@mail.com>
2019-07-12 16:24:49 -0400

    Initial commit.

---
+++ init.lua
@@ -0,0 +1,21 @@
+gdamage = {}
+
+-- List of registered damage types.
+gdamage.types = {}
+
+-- Register a damage type.
+function gdamage.register(type)
+	gdamage.types[type] = true
+	if minetest.get_modpath("armor_monoids") then
+		armor_monoids.register_armor_group(type, 100)
+	end
+end
+
+-- Get a list of default armor groups.
+function gdamage.armor_defaults()
+	local ret = {}
+	for k in pairs(gdamage.types) do
+		ret[k] = 100
+	end
+	return ret
+end
